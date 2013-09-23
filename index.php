c2d76ded569bb9ed5dddcf2470ef07db6c53670b
gitgoin <clistemail0000@gmail.com>
2013-09-23 00:29:08 -0400

    1

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+   xmlns:og="http://ogp.me/ns#"
+   xmlns:fb="https://www.facebook.com/2008/fbml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <script type="text/javascript" src="js/jquery.min.js"></script>
+      <script type="text/javascript" src="js/jquery.easing.1.3.min.js"></script>
+      <script type="text/javascript" src="js/fb.js"></script>
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <title>Facebook App - Lindt</title>
+   </head>
+   <body>
+      <div id="fb-root"></div>
+      <script src="http://connect.facebook.net/en_US/all.js"></script>
+      <script>
+         FB.init({
+           appId  : '657591667592073',
+           status : true, 
+           cookie : true, 
+           xfbml  : true  
+         });
+      </script>
+      <center>
+      <div class="fb-likeWrapper">
+         <div class="fb-like" data-href="http://www.tylerlombardi.com/c1/index.php" data-width="450" data-show-faces="false" data-send="true"></div>
+         </div>
+         <div id="contentWrapperWrapper">
+            <div id="contentWrapper">
+               <div id="content">
+               </div>
+            </div>
+         </div>
+      </center>
+      <div class="clear"></div>
+   </body>
+</html>
---
+++ nonfancontent.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>Untitled Document</title>
+   </head>
+   <body>
+      <div id="container">
+         <div id="upper2">
+            <div id="tryLindt">
+               <img src="img/try-lindt.png" alt="try lindt chocolate" title="try lindt chocolate" />
+            </div>
+         </div>
+         <div id="lowerWrapperWrapper">
+            <div id="lowerWrapper">
+               <div id="lower">
+                  <div id="lindtLogoWrapper">
+                     <div id="lindtLogo">
+                        <img src="img/lindt-logo.png" alt="lindt chocolate company" title="lindt chocolate company" />
+                     </div>
+                     <div id="chocBeyond">
+                        <img src="img/chocolate-beyond.png" alt="chocolate beyond compare" title="chocolate beyond compare" />
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div id="footer">
+            <ul>
+               <li><a href="#">Privacy Policy</a></li>
+               <li class="footerNavDiv">|</li>
+               <li><a href="#">Contact Us</a></li>
+               <li class="footerNavDiv">|</li>
+               <li><a href="#">FAQ</a></li>
+            </ul>
+            <div class="clear"></div>
+         </div>
+      </div>
+   </body>
+</html>
