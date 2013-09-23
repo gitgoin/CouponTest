2f2cd0b4e4f51bae12b1393da800bb4579369ab8
gitgoin <clistemail0000@gmail.com>
2013-09-23 00:34:56 -0400

    2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ CouponTest
 ==========
 
 C1 Coupon Test
+http://www.tylerlombardi.com/c1/index.php
---
+++ fancontent.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>Untitled Document</title>
+   </head>
+   <body>
+      <div id="container">
+         <div id="upper">
+            <div id="tryLindt">
+               <img src="img/try-lindt.png" alt="try lindt chocolate" title="try lindt chocolate" />
+            </div>
+            <div id="tasteTheDifference">
+               TASTE THE DIFFERENCE
+            </div>
+            <div id="vid">
+               <iframe width="737" height="420" src="http://www.youtube.com/embed/XpLtQDbnWEs?rel=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div id="products">
+               <div id="productImgs">
+                  <div id="excellenceWrapper" class="productImgWrapper">
+                     <div id="excellence" class="p1">
+                     </div>
+                  </div>
+                  <div id="milkWrapper" class="productImgWrapper">
+                     <div id="milk" class="p2">
+                     </div>
+                  </div>
+                  <div id="classicWrapper" class="productImgWrapper">
+                     <div id="classic" class="p3">
+                     </div>
+                  </div>
+               </div>
+               <div id="productInfoWrapper">
+                  <div class="bgLine">
+                  </div>
+                  <div class="bgLine2">
+                  </div>
+                  <div id="productInfo">
+                     <div class="pI1">
+                        Product #1 Info Product #1 Info Product #1 Info Product #1 Info Product #1 Info Product #1 Info Product #1 Info Product #1 Info Product #1 Info
+                     </div>
+                     <div class="pI2">
+                        Product #2 Info Product #2 Info Product #2 Info Product #2 Info Product #2 Info Product #2 Info Product #2 Info Product #2 Info Product #2 Info
+                     </div>
+                     <div class="pI3">
+                        Product #3 Info Product #3 Info Product #3 Info Product #3 Info Product #3 Info Product #3 Info Product #3 Info Product #3 Info Product #3 Info
+                     </div>
+                     <div class="pI4">
+                        When the video ends you will be redirected to download your $2.00 coupon valid on any ONE (1) Lindt product (3.5 oz or greater).
+                     </div>
+                  </div>
+                  <div class="bgLine2">
+                  </div>
+                  <div class="bgLine">
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div id="lowerWrapperWrapper">
+            <div id="lowerWrapper">
+               <div id="lower">
+                  <div id="lindtLogoWrapper">
+                     <div id="lindtLogo">
+                        <img src="img/lindt-logo.png" alt="lindt chocolate company" title="lindt chocolate company" />
+                     </div>
+                     <div id="chocBeyond">
+                        <img src="img/chocolate-beyond.png" alt="chocolate beyond compare" title="chocolate beyond compare" />
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div id="footer">
+            <ul>
+               <li><a href="#">Privacy Policy</a></li>
+               <li class="footerNavDiv">|</li>
+               <li><a href="#">Contact Us</a></li>
+               <li class="footerNavDiv">|</li>
+               <li><a href="#">FAQ</a></li>
+            </ul>
+            <div class="clear"></div>
+         </div>
+      </div>
+   </body>
+</html>
